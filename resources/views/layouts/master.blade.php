2f23b413edd4a8571e0c3afd64f080322eab3af4
cluk3 <luca.campli@student.unife.it>
2016-03-19 23:30:15 +0100

    Add navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,6 +21,20 @@
 		</style>
 	</head>
 	<body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <h1>PickUpThePlanet</h1>
+          </a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Notifiche</a></li>
+          <li><a href="#">Ricerca</a></li>
+          <li><a href="#">Scrivi</a></li>
+        </ul>
+      </div>
+    </nav>
 		<div class="container">
 			@yield('content')
 		</div>

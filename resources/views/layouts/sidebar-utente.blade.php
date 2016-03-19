ed2cbd20e97f0cd06b607a21ea1e67cdc37fe4c8
cluk3 <luca.campli@student.unife.it>
2016-03-20 00:38:19 +0100

    c'ho sonno

--- resources/views/layouts/sidebar-utente.blade.php
+++ resources/views/layouts/sidebar-utente.blade.php
@@ -1,7 +1,13 @@
-<div class="col-sm-3">
-  <ul class="list-group">
-    <li class="list-group-item">{{ $user['name'] }}</li>
-    <li class="list-group-item">{{ $user['city'] }}</li>
-    <li class="list-group-item">{{ $user['description'] }}</li>
-  </ul>
+<div class="col-sm-3" >
+  <img src="{{ assets('public/img/'.$user['image']) }}" alt="{{ $user['name'] }}-avatar" class="img-circle center-block">
+  <h2 class="text-center">{{ $user['name'] }}</h2>
+  <h3 class="text-center">{{ $user['username'] }}
+  <div class="row">
+    <div class="col-sm-6">
+      <p>Followers</p>
+    </div>
+    <div class="col-sm-6">
+      <p>Following</p>
+    </div>
+  </div>
 </div>

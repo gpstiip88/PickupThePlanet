19314c60117897f3f0e60ae6d18136a7dca6ba6f
cluk3 <luca.campli@student.unife.it>
2016-03-20 00:44:11 +0100

    c'ho tanto sonno

--- resources/views/layouts/sidebar-utente.blade.php
+++ resources/views/layouts/sidebar-utente.blade.php
@@ -1,7 +1,8 @@
 <div class="col-sm-3" >
-  <img src="{{ assets('public/img/'.$user['image']) }}" alt="{{ $user['name'] }}-avatar" class="img-circle center-block">
+  <img src="{{ assets('public/img/'.$user['image']) }}"
+    alt="{{ $user['name'] }}-avatar" class="img-circle center-block">
   <h2 class="text-center">{{ $user['name'] }}</h2>
-  <h3 class="text-center">{{ $user['username'] }}
+  <h3 class="text-center">{{ $user['username'] }}</h3>
   <div class="row">
     <div class="col-sm-6">
       <p>Followers</p>

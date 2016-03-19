b4c4dad39a947794badd7430fc4b1383c3dacd7c
Gabriele Pieretti <gabriele.pieretti@gmail.com>
2016-03-20 00:34:23 +0100

    relazione morph dei commenti e aggiunta immagine a utenti

--- app/Comment.php
+++ app/Comment.php
@@ -14,7 +14,8 @@ class Comment extends Model
         return $this->belongsTo('App\User');
     }
 
-    public function post(){
-        return $this->belongsTo('App\Post');
+    public function commentable()
+    {
+        return $this->morphTo();
     }
 }
--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'level', 'city', 'description'
+        'name', 'email', 'password', 'level', 'city', 'description', 'image'
     ];
 
     /**

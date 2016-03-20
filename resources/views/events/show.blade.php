2d4d1c475dd87e33d9d1e1421d260367cffd3618
cluk3 <luca.campli@student.unife.it>
2016-03-20 07:20:57 +0100

    mi scappa la cacca

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="row">
-    <div class="col-sm-4">
-        <div class="same-height sfondo-arancio testo-bianco testo-grande text-uppercase">
-          tipo evento
-        </div>
-    </div>
-    <div class="col-sm-8">
-      <h2>{{ $event['title'] }}</h2>
-      <h3>{{ $event['date'] }}</h3>
-      <img src="{{ asset($event->user->image) }}" class="mini-avatar img-circle pull-left">
-      <p>Organizzato da {{ $event->user->name }}</p>
-    </div>
+  <div class="sfondo-bianco">
     <div class="row">
+      <div class="col-sm-4">
+          <div class="same-height sfondo-arancio testo-bianco testo-grande text-uppercase">
+            tipo evento
+          </div>
+      </div>
+      <div class="col-sm-8">
+        <h2>{{ $event['title'] }}</h2>
+        <h3>{{ $event['date'] }}</h3>
+        <img src="{{ asset($event->user->image) }}" class="mini-avatar img-circle pull-left">
+        <p>Organizzato da {{ $event->user->name }}</p>
+      </div>
+    </div>
+    <div class="row" style="border-bottom: 1px solid orange">
       <div class="col-sm-8">
         <p>{{ $event->description }}</p>
       </div>
@@ -26,6 +28,5 @@
         @endforeach
       </div>
     </div>
-
   </div>
 @stop

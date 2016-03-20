f531a9bd433c9cdf2f2d6fb75705d559eeed6ddd
cluk3 <luca.campli@student.unife.it>
2016-03-20 01:47:05 +0100

    voglio una redbull

--- resources/views/layouts/sidebar-utente.blade.php
+++ resources/views/layouts/sidebar-utente.blade.php
@@ -1,14 +1,16 @@
-<div class="col-sm-3" >
+<div idclass="col-sm-3 center-block">
   <img src="{{ asset('public/img/'.$user['image']) }}"
-    alt="{{ $user['name'] }}-avatar" class="img-circle center-block">
+    alt="{{ $user['name'] }}-avatar" class="img-circle">
   <h2 class="text-center">{{ $user['name'] }}</h2>
   <h3 class="text-center">{{ $user['username'] }}</h3>
   <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-6 followers">
       <p>Followers</p>
+      <span id="followers-number">120</span>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-6 following">
       <p>Following</p>
+      <span id="following-number">88</span>
     </div>
   </div>
 </div>

36fc3daebee2faa9a5b3c8f8126322be8fd5ee4a
Gabriele Pieretti <gabriele.pieretti@gmail.com>
2016-03-20 02:04:13 +0100

    fix bug sidebar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#"><img alt="Brand" src="{{ asset('public/img/logo.png') }}"></a>
+      <a class="navbar-brand" href="#"><img alt="Brand" src="{{ asset('img/logo.png') }}"></a>
     </div>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#">Notifiche</a></li>
--- resources/views/layouts/sidebar-utente.blade.php
+++ resources/views/layouts/sidebar-utente.blade.php
@@ -1,6 +1,5 @@
 <div id="sidebar-utente" class="col-sm-3 center-block">
-  <img src="{{ asset('public/img/utente.jpg' }}"
-    alt="{{ $user['name'] }}-avatar" class="img-circle">
+  <img src="{{ asset('img/utente.jpg') }}" alt="{{ $user['name'] }}-avatar" class="img-circle">
   <h2 class="text-center">{{ $user['name'] }}</h2>
   <h3 class="text-center">{{ $user['username'] }}</h3>
   <div class="row">
